d633b02bb5aea85a168f933205ad5227638f0a0a
evgvalo <valov.eu@yandex.ru>
2021-12-10 20:34:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from PySide2 import QtCore, QtWidgets
+from practice2 import Ui_Form
+from functools import partial
+
+
+class Practice2(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(Practice2, self).__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.pushbutton_move)
+        self.ui.pushButton_2.clicked.connect(self.pushbutton_move)
+        self.ui.pushButton_3.clicked.connect(self.pushbutton_move)
+        self.ui.pushButton_4.clicked.connect(self.pushbutton_move)
+        self.ui.pushButton_5.clicked.connect(self.pushbutton_move)
+        self.ui.pushButton_6.clicked.connect(self.pushbutton_params)
+        self.ui.pushButton_6.setShortcut('F1')
+
+
+
+    def pushbutton_move(self):
+        screenWidth = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+        screenHeight = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+        if self.sender().objectName() == 'pushButton':
+            self.move(0, 0)
+        if self.sender().objectName() == 'pushButton_2':
+            self.move(screenWidth - self.width(), 0)
+        if self.sender().objectName() == 'pushButton_3':
+            self.move(screenWidth / 2 - self.width() / 2, screenHeight / 2 - self.height() / 2)
+        if self.sender().objectName() == 'pushButton_4':
+            self.move(0, screenHeight - self.height())
+        if self.sender().objectName() == 'pushButton_5':
+            self.move(screenWidth - self.width(), screenHeight - self.height())
+
+    def pushbutton_params(self):
+        self.ui.plainTextEdit.appendPlainText(str(len(QtWidgets.QApplication.screens())))
+        self.ui.plainTextEdit.appendPlainText(QtWidgets.QApplication.primaryScreen().name())
+        self.ui.plainTextEdit.appendPlainText(str(QtWidgets.QApplication.screenAt(self.pos()).size()))
+        self.ui.plainTextEdit.appendPlainText(QtWidgets.QApplication.screenAt(self.pos()).name())
+        self.ui.plainTextEdit.appendPlainText(str(self.size()))
+        self.ui.plainTextEdit.appendPlainText(str(self.minimumSize()))
+        self.ui.plainTextEdit.appendPlainText(str(self.pos()))
+        self.ui.plainTextEdit.appendPlainText(f'{self.pos().x()+self.width() / 2}, {self.pos().y()+self.height() / 2}')
+
+    def event(self, event:QtCore.QEvent):
+        print(event.type())
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+    win = Practice2()
+    win.show()
+    app.exec_()
---
+++ practice2.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Практика2.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(838, 512)
+        self.gridLayout_2 = QGridLayout(Form)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalLayout_6.setSizeConstraint(QLayout.SetMinAndMaxSize)
+        self.horizontalLayout_6.setContentsMargins(0, -1, -1, -1)
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.pushButton_4 = QPushButton(Form)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
+        self.pushButton_4.setSizePolicy(sizePolicy)
+        self.pushButton_4.setMinimumSize(QSize(50, 75))
+
+        self.horizontalLayout_3.addWidget(self.pushButton_4)
+
+        self.pushButton_5 = QPushButton(Form)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        sizePolicy.setHeightForWidth(self.pushButton_5.sizePolicy().hasHeightForWidth())
+        self.pushButton_5.setSizePolicy(sizePolicy)
+        self.pushButton_5.setMinimumSize(QSize(50, 75))
+
+        self.horizontalLayout_3.addWidget(self.pushButton_5)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_3, 2, 0, 1, 1)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.pushButton_3 = QPushButton(Form)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        sizePolicy.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
+        self.pushButton_3.setSizePolicy(sizePolicy)
+        self.pushButton_3.setMinimumSize(QSize(50, 75))
+
+        self.horizontalLayout_2.addWidget(self.pushButton_3)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_2, 1, 0, 1, 1)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.dial = QDial(Form)
+        self.dial.setObjectName(u"dial")
+        self.dial.setMinimumSize(QSize(200, 200))
+
+        self.horizontalLayout_5.addWidget(self.dial)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.comboBox = QComboBox(Form)
+        self.comboBox.setObjectName(u"comboBox")
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setMinimumSize(QSize(200, 0))
+
+        self.verticalLayout_2.addWidget(self.comboBox)
+
+        self.lcdNumber = QLCDNumber(Form)
+        self.lcdNumber.setObjectName(u"lcdNumber")
+        sizePolicy.setHeightForWidth(self.lcdNumber.sizePolicy().hasHeightForWidth())
+        self.lcdNumber.setSizePolicy(sizePolicy)
+        self.lcdNumber.setMinimumSize(QSize(200, 180))
+
+        self.verticalLayout_2.addWidget(self.lcdNumber)
+
+
+        self.horizontalLayout_5.addLayout(self.verticalLayout_2)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_5, 4, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMinimumSize(QSize(50, 75))
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(Form)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setMinimumSize(QSize(50, 75))
+
+        self.horizontalLayout.addWidget(self.pushButton_2)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.pushButton_6 = QPushButton(Form)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        sizePolicy.setHeightForWidth(self.pushButton_6.sizePolicy().hasHeightForWidth())
+        self.pushButton_6.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout_4.addWidget(self.pushButton_6)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_4, 3, 0, 1, 1)
+
+        self.horizontalSlider = QSlider(Form)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+
+        self.gridLayout.addWidget(self.horizontalSlider, 5, 0, 1, 1)
+
+
+        self.horizontalLayout_6.addLayout(self.gridLayout)
+
+        self.plainTextEdit = QPlainTextEdit(Form)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.setMinimumSize(QSize(400, 0))
+
+        self.horizontalLayout_6.addWidget(self.plainTextEdit)
+
+
+        self.gridLayout_2.addLayout(self.horizontalLayout_6, 0, 0, 1, 1)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Form", u"\u041b\u0435\u0432\u043e/\u041d\u0438\u0437", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Form", u"\u041f\u0440\u0430\u0432\u043e/\u041d\u0438\u0437", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Form", u"\u0426\u0435\u043d\u0442\u0440", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"\u041b\u0435\u0432\u043e/\u0412\u0435\u0440\u0445", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Form", u"\u041f\u0440\u0430\u0432\u043e/\u0412\u0435\u0440\u0445", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u0434\u0430\u043d\u043d\u044b\u0435 \u043f\u043e\u0442\u043e\u043a\u0430", None))
+    # retranslateUi
+
